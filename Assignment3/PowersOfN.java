145e7e0e9df81fe637be786c5a2258f36b4ad432
Craig Peterson <craig.b.peterson@gmail.com>
2019-01-14 15:31:48 -0800

    upload assignment 3: printPowersOfN

---
+++ Assignment3/PowersOfN.java
@@ -0,0 +1,24 @@
+
+public class PowersOfN {
+
+	public static void main(String[] args) {
+		printPowersOfN(2, 7);
+		System.out.println();
+		printPowersOfN(5, 3);
+		System.out.println();
+		printPowersOfN(4, 3);
+		System.out.println();
+		printPowersOfN(5, 6);
+		System.out.println();
+		printPowersOfN(-2, 8);
+		System.out.println();
+		
+	}
+
+	public static void printPowersOfN(int base, int maxExp) {
+		for (int i = 0; i <= maxExp; i++) {
+			int powResult = (int) Math.pow(base, i);
+			System.out.print(powResult + " ");
+		}
+	}
+}
