557b896e77b299badb9c6e3222eaabe477045951
Craig Peterson <craig.b.peterson@gmail.com>
2019-01-30 17:29:01 -0800

    switched to using console.next() to parse user input
    in the previous version I was splitting the string into an array, which was a strategy not covered in this chapter of the text

--- Assignment5/GPA.java
+++ Assignment5/GPA.java
@@ -4,25 +4,33 @@ public class GPA {
 
 	public static void main(String[] args) {
 		Scanner console = new Scanner(System.in);
-
+		
+		System.out.println("GRADE CALCULATOR");
+		System.out.println("To calculate a grade, enter the student");
+		System.out.println("record on one line starting with the");
+		System.out.println("name, then the number of grades to average,");
+		System.out.println("then each grade separated by spaces.");
+		System.out.println();
+		System.out.println("For example, if Maria has 5 grades to");
+		System.out.println("average, her record would look like this:");
+		System.out.println();
+		System.out.println("Maria 5 72 91 84 89 78");
+		System.out.println();
+		
 		System.out.println(printGPA(console));
 	}
 	
 	public static String printGPA(Scanner console) {
 		System.out.print("Enter a student record: "); //prompt user
-		String record = console.nextLine(); //get input from user
-		
-		record = record.trim(); //remove white space from beginning and end of user input string, if necessary
-		String[] recordArray = record.split(" "); //creates an array from the string, splitting on each space
-		
-		String studentName = recordArray[0]; //get student name
-		int numOfScores = Integer.parseInt(recordArray[1]); //get number of scores
+
+		String studentName = console.next(); //get name from user input
+		int numOfScores = Integer.parseInt(console.next()); //get number of scores to average from user input
 		
 		//get sum of all scores
 		int sum = 0;
 		int score;
-		for (int i = 2; i < recordArray.length; i++) { //start at array index 2 for the first score
-			score = Integer.parseInt(recordArray[i]); //convert each score from string to int
+		for (int i = 1; i <= numOfScores; i++) {
+			score = Integer.parseInt(console.next()); //convert each score from string to int
 			sum += score; //add up all scores from the array
 		}
 		
