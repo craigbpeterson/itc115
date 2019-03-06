e88efbe4df81a8ada646904110cb1a8fb96f7668
Craig Peterson <craig.b.peterson@gmail.com>
2019-03-06 09:16:55 -0800

    revised getCount() to be a static method

--- Assignment9/UseProduct.java
+++ Assignment9/UseProduct.java
@@ -2,30 +2,22 @@
 public class UseProduct {
 
 	public static void main(String[] args) {
-		int totalProductCount = 0;
-		
-		Product shirt = new Product(1, "T-Shirt", 14.99, 12);
-		totalProductCount += shirt.getProductCount();
-		
-		Product pants = new Product(2, "Jeans", 39.99, 8);
-		totalProductCount += pants.getProductCount();
-		
-		Product shoes = new Product(3, "Sneakers", 60, 7);
-		totalProductCount += shoes.getProductCount();
 
+		Product shirt = new Product(1, "T-Shirt", 19.99, 12);
+		Product pants = new Product(2, "Jeans", 39.99, 8);
+		Product shoes = new Product(3, "Sneakers", 60, 7);
+		Product hat = new Product(4, "Baseball Cap", 12.5, 22);
+		
 		//display results of product instantiation
 		System.out.println("All product info:");
 		System.out.println(shirt.toString());
 		System.out.println(pants.toString());
 		System.out.println(shoes.toString());
+		System.out.println(hat.toString());
 		
-		//report quantity of entire inventory
-		getCount(totalProductCount);
-	}
-	
-	//prints a statement about the total quantity of items in the inventory
-	public static void getCount(int totalProductCount) {
-		System.out.println("The total quantity of items in the inventory: " + totalProductCount);
+		//report on number of Product instantiations
+		System.out.println();
+		System.out.println("There are " + Product.getCount() + " objects of the Product class.");
 	}
 
 }
