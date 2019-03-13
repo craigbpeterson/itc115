62f4cdcaa5780781ca1d7135e0110d0d4d128df2
Craig Peterson <craig.b.peterson@gmail.com>
2019-03-13 15:12:35 -0700

    upload Employee superclass

---
+++ Assignment11/Employee.java
@@ -0,0 +1,20 @@
+//represents employees in general
+public class Employee {
+	
+	/* METHODS */
+	public String getVacationForm() {
+		return "yellow";
+	}
+	
+	public int getHours() {
+		return 40;
+	}
+	
+	public double getSalary() {
+		return 40000.0;
+	}
+	
+	public int getVacationDays() {
+		return 10;
+	}
+}
