d719b2150f8c000247648d20d071f39abfd5ae22
Craig Peterson <craig.b.peterson@gmail.com>
2019-03-13 15:13:02 -0700

    upload Secretary subclass

---
+++ Assignment11/Secretary.java
@@ -0,0 +1,8 @@
+//represents secretaries
+public class Secretary extends Employee {
+
+	/* METHODS */
+	public void takeDictation(String text) {
+		System.out.println("Taking dictation of text: " + text);
+	}
+}
